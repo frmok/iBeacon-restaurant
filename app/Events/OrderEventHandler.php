7d8691faa1b7629c699c36174688409bf411449d
frmok <baalmok@gmail.com>
2015-04-01 17:21:38 +0800

    Websocket bug

--- app/Events/OrderEventHandler.php
+++ app/Events/OrderEventHandler.php
@@ -14,11 +14,11 @@ class OrderEventHandler {
         $socket->connect("tcp://127.0.0.1:".env('ZEROMQ_PORT'));
         $socket->send(json_encode($packet));
 
-        
+
         //for mobile bill update
         $packet = array();
         $packet['action'] = 'wsorder.refresh';
-        $packet['billId'] = $order->bill->id;
+        $packet['billId'] = $order->bill_id;
         $context = new \ZMQContext(1, false);
         $socket = $context->getSocket(\ZMQ::SOCKET_PUSH, 'new ticket');
         $socket->connect("tcp://127.0.0.1:".env('ZEROMQ_PORT'));
